fbae9220e6a87374624a9b10c486e4d8f1dbeeb0
Yamaguchi0412 <2401113@s.asojuku.ac.jp>
2025-05-29 10:57:18 +0900

    ログイン画面の追加

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>ログイン</h1>
+    <p>ユーザー名：<input type="text" name="username" required></p><br>
+    <p>パスワード：<input type="password" name="password" required></p><br>
+    <input type="submit" value="ログイン">
+</body>
+</html>
